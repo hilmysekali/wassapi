2d4bbe6aa95d7a6a0e0e1b7a2d2eb7fcf6f33b07
hilmysekali <zulhilmidharma@gmail.com>
2021-09-14 11:10:00 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
-const { Client } = require('whatsapp-web.js');
+const { Client, MessageMedia } = require('whatsapp-web.js');
 const express = require('express');
 const { body, validationResult } = require('express-validator');
 const socketIO = require('socket.io');
 const qrcode = require('qrcode');
 const http = require('http');
+const fs = require('fs');
 const { phoneNumberFormatter } = require('./helpers/formatter');
+const request = require('request');
 
 const port = process.env.PORT || 8000;
 const tokennya = '71f7be7b8496f7ece8454b1bcdcd2162';
@@ -14,152 +16,219 @@ const server = http.createServer(app);
 const io = socketIO(server);
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({
+  extended: true
+}));
 
-const db = require('./helpers/db.js');
+const SESSION_FILE_PATH = './whatsapp-session.json';
+let sessionCfg;
+if (fs.existsSync(SESSION_FILE_PATH)) {
+  sessionCfg = require(SESSION_FILE_PATH);
+}
 
-(async() => {
-	app.get('/', (req, res) => {
-		res.sendFile('index.html', { 
-			root: __dirname });
-	});
+app.get('/', (req, res) => {
+  res.sendFile('index.html', {
+    root: __dirname
+  });
+});
 
-	const savedSession = await db.readSession();
-	const client = new Client({
-		restartOnAuthFail: true,
-		puppeteer: {
-			headless: true,
-			args: [
-				'--no-sandbox',
-				'--disable-setuid-sandbox',
-				'--disable-dev-shm-usage',
-				'--disable-accelerated-2d-canvas',
-				'--no-first-run',
-				'--no-zygote',
-				'--single-process',
-				'--disable-gpu'
-				],
-		},
-		session: savedSession
-	});
+const client = new Client({
+  restartOnAuthFail: true,
+  puppeteer: {
+    headless: true,
+    args: [
+      '--no-sandbox',
+      '--disable-setuid-sandbox',
+      '--disable-dev-shm-usage',
+      '--disable-accelerated-2d-canvas',
+      '--no-first-run',
+      '--no-zygote',
+      '--single-process', // <- this one doesn't works in Windows
+      '--disable-gpu'
+    ],
+  },
+  session: sessionCfg
+});
 
-	client.on('message', msg => {
-	    if (msg.body == '!cek_saldo') {
-	        msg.reply('Masukkan Nomor Rekening');
-		client.on('message', cek => {
-		  if(cek.body == '120'){
-		     cek.reply('Nomor Rekening : *120*\n Saldo anda : *Rp.100,000*');
-		  } else{
-		     cek.reply('Nomor Rekening tidak ada.\nSilahkan periksa kembali Nomor Rekening anda!');
-		     return false;
-		  }
-		});
-		  return true;
-	    } else if (msg.body == 'reinvolve') {
-	    	msg.reply('Halo, Selamat datang!');
-	    } else if (msg.body == '!help') {
-	    	msg.reply('List perintah BOT\n1. *!cek_saldo*\n2. *hilmy*\n3. *reinvolve*');
-	    } else {
-	    	msg.reply('Selamat datang di Wassap BOT by Reinvolve versi Beta\nKetik *!help* untuk mengetahui beberapa perintah BOT.');
-	    }
-	});
+client.on('message', async msg => {
+	const { chat, from, caption } = msg
+	let { body } = msg
+    const commands = caption || body || ''
+    const command = commands.toLowerCase().split(' ')[0] || ''
+    const args =  commands.split(' ')
+   
+    const msgs = (msg) => {
+        if (command.startsWith('!')) {
+            if (msg.length >= 10){
+                return `${msg.substr(0, 15)}`
+            }else{
+                return `${msg}`
+            }
+        }
+    }
 
-	client.initialize();
+    switch(command) {
+    	case '!cek_saldo':
+    		if (args.length === 1) return msg.reply('Kirim perintah *!cek_saldo [nomor_rekening]*, contoh *!cek 45683*')
+    		const norek = body.slice(11)
+			request('https://reinvolve.online/api.php', function (error, response, body) {
+	  		if (!error && response.statusCode == 200) {
+		     	const rows = JSON.parse(body);
+		     	// console.log(rows[1].no_rekening);
 
-  io.on('connection', function(socket) {
-    socket.emit('message', 'Connecting...');
-  
-    client.on('qr', (qr) => {
-      console.log('QR RECEIVED', qr);
-      qrcode.toDataURL(qr, (err, url) => {
-        socket.emit('qr', url);
-        socket.emit('message', 'QR Code received, scan please!');
-      });
-    });
-  
-    client.on('ready', () => {
-      socket.emit('ready', 'Whatsapp is ready!');
-      socket.emit('message', 'Whatsapp is ready!');
-    });
-  
-    client.on('authenticated', (session) => {
-      socket.emit('authenticated', 'Whatsapp is authenticated!');
-      socket.emit('message', 'Whatsapp is authenticated!');
-      console.log('AUTHENTICATED', session);
-      // Save session to DB
-      db.saveSession(session);
-    });
-  
-    client.on('auth_failure', function(session) {
-      socket.emit('message', 'Auth failure, restarting...');
-    });
-  
-    client.on('disconnected', (reason) => {
-      socket.emit('message', 'Whatsapp is disconnected!');
-      // Remove session from DB
-      db.removeSession();
-      client.destroy();
-      client.initialize();
+		     	const cari = norek;
+			 	const x = rows.length;
+			 	// console.log(x);
+
+		    	for (var i = 0; i < x; i++) {
+		    	// console.log(rows[i].nama_nasabah);
+			    	if (rows[i].no_rekening == cari) {
+			    		// console.log(rows[i].nama_nasabah);
+			    		var find = 1;
+			    		var no_rekening = rows[i].no_rekening;
+			    		var nama_nasabah = rows[i].nama_nasabah;
+			    		var alamat = rows[i].alamat;
+			    		var no_hp = rows[i].no_hp;
+			    		var nama_orang_tua = rows[i].nama_orang_tua;
+			    		var id_kelas = rows[i].id_kelas;
+			    	}
+		    	}
+			}
+		    if (find == 1) {
+		    	const pesan = '*CEK SALDO*\nNo. Rekening : *'+no_rekening+'*\nNama Nasabah : *'+nama_nasabah+'*\nAlamat : *'+alamat+'*\nNo. HP : *'+no_hp+'*\nNama Orang Tua : *'+nama_orang_tua+'*\nKelas : *'+id_kelas+'*\nJumlah Saldo : *Rp.500,000*';
+		    	msg.reply(pesan);
+			}
+			else {
+				// const pesan = 'Nomor Rekening tidak ditemukan';
+				msg.reply('Nomor Rekening tidak ditemukan');
+			}
+		})
+    	break
+
+    	case '!reinvolve':
+    	case 'hilmy':
+    	case 'saya':
+    		client.sendMessage(from, 'Halo Selamat datang di *Reinvolve* Whatsapp BOT');
+    	break
+
+    	case '!test':
+    	case '!coba':
+    		msg.reply('Ini adalah perintah untuk testing');
+    	break
+
+    	case '!help':
+    	case '!bantuan':
+    		client.sendMessage(from, 'Perintah BOT\n1. *!cek_saldo*\n2. *!reinvolve*\n3. *!test*');
+    	break
+
+    	default:
+    		client.sendMessage(from, 'Selamat datang di Wassap BOT by Reinvolve versi Beta\nKetik *!help* untuk mengetahui beberapa perintah BOT.');
+    	break
+    }
+});
+
+client.initialize();
+
+// Socket IO
+io.on('connection', function(socket) {
+  socket.emit('message', 'Connecting...');
+
+  client.on('qr', (qr) => {
+    console.log('QR RECEIVED', qr);
+    qrcode.toDataURL(qr, (err, url) => {
+      socket.emit('qr', url);
+      socket.emit('message', 'QR Code received, scan please!');
     });
   });
 
-  const checkRegisteredNumber = async function(number) {
-    const isRegistered = await client.isRegisteredUser(number);
-    return isRegistered;
+  client.on('ready', () => {
+    socket.emit('ready', 'Whatsapp is ready!');
+    socket.emit('message', 'Whatsapp is ready!');
+  });
+
+  client.on('authenticated', (session) => {
+    socket.emit('authenticated', 'Whatsapp is authenticated!');
+    socket.emit('message', 'Whatsapp is authenticated!');
+    console.log('AUTHENTICATED', session);
+    sessionCfg = session;
+    fs.writeFile(SESSION_FILE_PATH, JSON.stringify(session), function(err) {
+      if (err) {
+        console.error(err);
+      }
+    });
+  });
+
+  client.on('auth_failure', function(session) {
+    socket.emit('message', 'Auth failure, restarting...');
+  });
+
+  client.on('disconnected', (reason) => {
+    socket.emit('message', 'Whatsapp is disconnected!');
+//     fs.unlinkSync(SESSION_FILE_PATH, function(err) {
+//         if(err) return console.log(err);
+//         console.log('Session file deleted!');
+//     });
+    client.destroy();
+    client.initialize();
+  });
+});
+
+
+const checkRegisteredNumber = async function(number) {
+  const isRegistered = await client.isRegisteredUser(number);
+  return isRegistered;
+}
+
+// Send message
+app.post('/send-message', [
+  body('token').notEmpty(),
+  body('number').notEmpty(),
+  body('message').notEmpty(),
+], async (req, res) => {
+  const errors = validationResult(req).formatWith(({
+    msg
+  }) => {
+    return msg;
+  });
+	
+  if (tokennya != req.body.token) {
+   return res.status(422).json({
+   status: false,
+   message: 'Token Tidak Valid!'
+   });
   }
-  
-  // Send message
-  app.post('/send-message', [
-    body('token').notEmpty(),
-    body('number').notEmpty(),
-    body('message').notEmpty(),
-  ], async (req, res) => {
-    const errors = validationResult(req).formatWith(({
-      msg
-    }) => {
-      return msg;
+  if (!errors.isEmpty()) {
+    return res.status(422).json({
+      status: false,
+      message: errors.mapped()
     });
-	  
-    if (tokennya != req.body.token) {
-    	return res.status(422).json({
-		status: false,
-		message: 'Token Tidak Valid!'
-	});
-    }
-  
-    if (!errors.isEmpty()) {
-      return res.status(422).json({
-        status: false,
-        message: errors.mapped()
-      });
-    }
-  
-    const number = phoneNumberFormatter(req.body.number);
-    const message = req.body.message;
-  
-    const isRegisteredNumber = await checkRegisteredNumber(number);
-  
-    if (!isRegisteredNumber) {
-      return res.status(422).json({
-        status: false,
-        message: 'The number is not registered'
-      });
-    }
-  
-    client.sendMessage(number, message).then(response => {
-      res.status(200).json({
-        status: true,
-        response: response
-      });
-    }).catch(err => {
-      res.status(500).json({
-        status: false,
-        response: err
-      });
+  }
+
+  const number = phoneNumberFormatter(req.body.number);
+  const message = req.body.message;
+
+  const isRegisteredNumber = await checkRegisteredNumber(number);
+
+  if (!isRegisteredNumber) {
+    return res.status(422).json({
+      status: false,
+      message: 'The number is not registered'
+    });
+  }
+
+  client.sendMessage(number, message).then(response => {
+    res.status(200).json({
+      status: true,
+      response: response
+    });
+  }).catch(err => {
+    res.status(500).json({
+      status: false,
+      response: err
     });
   });
+});
 
 server.listen(port, function() {
-	console.log('App running on *: ' + port);
+  console.log('App running on *: ' + port);
 });
-})();
