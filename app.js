37c80ef43724069c163ce006a20c040f340d51c8
hilmysekali <zulhilmidharma@gmail.com>
2021-09-15 16:45:07 +0700

    Update app.js

--- app.js
+++ app.js
@@ -22,12 +22,14 @@ app.use(express.urlencoded({
 
 const db = require('./helpers/db.js');
 
+(async() => {
   app.get('/', (req, res) => {
     res.sendFile('index.html', {
       root: __dirname
     });
   });
 
+  const savedSession = await db.readSession();
   const client = new Client({
     restartOnAuthFail: true,
     puppeteer: {
@@ -47,8 +49,8 @@ const db = require('./helpers/db.js');
   });
 
   client.on('message', async msg => {
-    const { chat, from, caption } = msg
-    let { body } = msg
+  	const { chat, from, caption } = msg
+  	let { body } = msg
       const commands = caption || body || ''
       const command = commands.toLowerCase().split(' ')[0] || ''
       const args =  commands.split(' ')
@@ -64,107 +66,105 @@ const db = require('./helpers/db.js');
       }
 
       switch(command) {
-        case '!cek_saldo':
-          if (args.length === 1) return msg.reply('Kirim perintah *!cek_saldo [nomor_rekening]*, contoh *!cek 45683*')
-          const norek = body.slice(11)
-        request('https://reinvolve.online/api.php', function (error, response, body) {
-          if (!error && response.statusCode == 200) {
-            const rows = JSON.parse(body);
-            // console.log(rows[1].no_rekening);
+      	case '!cek_saldo':
+      		if (args.length === 1) return msg.reply('Kirim perintah *!cek_saldo [nomor_rekening]*, contoh *!cek 45683*')
+      		const norek = body.slice(11)
+  			request('https://reinvolve.online/api.php', function (error, response, body) {
+  	  		if (!error && response.statusCode == 200) {
+  		     	const rows = JSON.parse(body);
+  		     	// console.log(rows[1].no_rekening);
 
-            const cari = norek;
-          const x = rows.length;
-          // console.log(x);
+  		     	const cari = norek;
+  			 	const x = rows.length;
+  			 	// console.log(x);
 
-            for (var i = 0; i < x; i++) {
-            // console.log(rows[i].nama_nasabah);
-              if (rows[i].no_rekening == cari) {
-                // console.log(rows[i].nama_nasabah);
-                var find = 1;
-                var no_rekening = rows[i].no_rekening;
-                var nama_nasabah = rows[i].nama_nasabah;
-                var alamat = rows[i].alamat;
-                var no_hp = rows[i].no_hp;
-                var nama_orang_tua = rows[i].nama_orang_tua;
-                var nama_kelas = rows[i].nama_kelas;
-            var total = rows[i].total;
-              }
-            }
-        }
-          if (find == 1) {
-            const pesan = '*CEK SALDO*\nNo. Rekening : *'+no_rekening+'*\nNama Nasabah : *'+nama_nasabah+'*\nAlamat : *'+alamat+'*\nNo. HP : *'+no_hp+'*\nNama Orang Tua : *'+nama_orang_tua+'*\nKelas : *'+id_kelas+'*\nJumlah Saldo : *Rp. '+total+'*';
-            msg.reply(pesan);
-        }
-        else {
-          // const pesan = 'Nomor Rekening tidak ditemukan';
-          msg.reply('Nomor Rekening tidak ditemukan');
-        }
-      })
-        break
+  		    	for (var i = 0; i < x; i++) {
+  		    	// console.log(rows[i].nama_nasabah);
+  			    	if (rows[i].no_rekening == cari) {
+  			    		// console.log(rows[i].nama_nasabah);
+  			    		var find = 1;
+  			    		var no_rekening = rows[i].no_rekening;
+  			    		var nama_nasabah = rows[i].nama_nasabah;
+  			    		var alamat = rows[i].alamat;
+  			    		var no_hp = rows[i].no_hp;
+  			    		var nama_orang_tua = rows[i].nama_orang_tua;
+  			    		var nama_kelas = rows[i].nama_kelas;
+  					var total = rows[i].total;
+  			    	}
+  		    	}
+  			}
+  		    if (find == 1) {
+  		    	const pesan = '*CEK SALDO*\nNo. Rekening : *'+no_rekening+'*\nNama Nasabah : *'+nama_nasabah+'*\nAlamat : *'+alamat+'*\nNo. HP : *'+no_hp+'*\nNama Orang Tua : *'+nama_orang_tua+'*\nKelas : *'+id_kelas+'*\nJumlah Saldo : *Rp. '+total+'*';
+  		    	msg.reply(pesan);
+  			}
+  			else {
+  				// const pesan = 'Nomor Rekening tidak ditemukan';
+  				msg.reply('Nomor Rekening tidak ditemukan');
+  			}
+  		});
+      	break
 
-        case '!reinvolve':
-    case 'reinvolve':
-        case 'hilmy':
-        case 'saya':
-          client.sendMessage(from, 'Halo Selamat datang di *Reinvolve* Whatsapp BOT');
-        break
+      	case '!reinvolve':
+  	case 'reinvolve':
+      	case 'hilmy':
+      	case 'saya':
+      		client.sendMessage(from, 'Halo Selamat datang di *Reinvolve* Whatsapp BOT');
+      	break
 
-        case '!test':
-        case '!coba':
-          msg.reply('Ini adalah perintah untuk testing');
-        break
+      	case '!test':
+      	case '!coba':
+      		msg.reply('Ini adalah perintah untuk testing');
+      	break
 
-        case '!help':
-        case '!bantuan':
-          client.sendMessage(from, 'Perintah BOT\n1. *!cek_saldo*\n2. *!reinvolve*\n3. *!test*');
-        break
+      	case '!help':
+      	case '!bantuan':
+      		client.sendMessage(from, 'Perintah BOT\n1. *!cek_saldo*\n2. *!reinvolve*\n3. *!test*');
+      	break
 
-        default:
-          client.sendMessage(from, 'Selamat datang di Wassap BOT by Reinvolve versi Beta\nKetik *!help* untuk mengetahui beberapa perintah BOT.');
-        break
+      	default:
+      		client.sendMessage(from, 'Selamat datang di Wassap BOT by Reinvolve versi Beta\nKetik *!help* untuk mengetahui beberapa perintah BOT.');
+      	break
       }
   });
 
   client.initialize();
 
-  // Socket IO
-    io.on('connection', function(socket) {
-      socket.emit('message', 'Connecting...');
-    
-      client.on('qr', (qr) => {
-        console.log('QR RECEIVED', qr);
-        qrcode.toDataURL(qr, (err, url) => {
-          socket.emit('qr', url);
-          socket.emit('message', 'QR Code received, scan please!');
-        });
-      });
-    
-      client.on('ready', () => {
-        socket.emit('ready', 'Whatsapp is ready!');
-        socket.emit('message', 'Whatsapp is ready!');
-      });
-    
-      client.on('authenticated', (session) => {
-        socket.emit('authenticated', 'Whatsapp is authenticated!');
-        socket.emit('message', 'Whatsapp is authenticated!');
-        console.log('AUTHENTICATED', session);
-        // Save session to DB
-        db.saveSession(session);
-      });
-    
-      client.on('auth_failure', function(session) {
-        socket.emit('message', 'Auth failure, restarting...');
-      });
-    
-      client.on('disconnected', (reason) => {
-        socket.emit('message', 'Whatsapp is disconnected!');
-        // Remove session from DB
-        // db.removeSession();
-        client.destroy();
-        client.initialize();
+  io.on('connection', function(socket) {
+    socket.emit('message', 'Connecting...');
+  
+    client.on('qr', (qr) => {
+      console.log('QR RECEIVED', qr);
+      qrcode.toDataURL(qr, (err, url) => {
+        socket.emit('qr', url);
+        socket.emit('message', 'QR Code received, scan please!');
       });
     });
-
+  
+    client.on('ready', () => {
+      socket.emit('ready', 'Whatsapp is ready!');
+      socket.emit('message', 'Whatsapp is ready!');
+    });
+  
+    client.on('authenticated', (session) => {
+      socket.emit('authenticated', 'Whatsapp is authenticated!');
+      socket.emit('message', 'Whatsapp is authenticated!');
+      console.log('AUTHENTICATED', session);
+      // Save session to DB
+      db.saveSession(session);
+    });
+  
+    client.on('auth_failure', function(session) {
+      socket.emit('message', 'Auth failure, restarting...');
+    });
+  
+    client.on('disconnected', (reason) => {
+      socket.emit('message', 'Whatsapp is disconnected!');
+      // Remove session from DB
+      db.removeSession();
+      client.destroy();
+      client.initialize();
+    });
+  });
 
   const checkRegisteredNumber = async function(number) {
     const isRegistered = await client.isRegisteredUser(number);
@@ -182,7 +182,7 @@ const db = require('./helpers/db.js');
     }) => {
       return msg;
     });
-    
+  	
     if (tokennya != req.body.token) {
      return res.status(422).json({
      status: false,
@@ -224,3 +224,4 @@ const db = require('./helpers/db.js');
   server.listen(port, function() {
     console.log('App running on *: ' + port);
   });
+})();
